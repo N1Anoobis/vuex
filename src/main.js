7603c62200405cf86869a70fa2c57a7543ad6319
N1Anoobis <mrbond84@hotmail.com>
2021-02-04 08:58:43 +0100

    Connect store in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
+import store from './store/modules/todos';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
